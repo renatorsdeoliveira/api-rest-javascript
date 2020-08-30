76dcd2bd91e7b96fa6a12655dc09322033605dc0
renato_O <renatorsdeoliveira@gmail.com>
2020-08-30 12:41:02 -0300

    Instalado o mogoose e passado a rota do banco

--- server.js
+++ server.js
@@ -1,7 +1,22 @@
-const express = require('express')
+const express = require("express")
+const mongoose = require("mongoose")
 
+
+// Iniciando o APP
 const app = express();
+
+
+// Iniciando o DB
+mongoose.connect("mongodb://localhost:27017/nodeapi", { useNewUrlParser: true,  useUnifiedTopology: true})
+
+
+
+// Primeira Rota
 app.get('/', (req, res) => {
-    res.send('Hello Word')
+    res.send("Hello teste")
 })
+
+
+
+// Porta
 app.listen(3000)
