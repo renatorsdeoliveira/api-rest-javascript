af9de92ad8b2c088160546fef91c945949e6d547
renato_O <renatorsdeoliveira@gmail.com>
2020-08-30 18:17:57 -0300

    Criado os metodos put e delete

--- src/controller/ProductController.js
+++ src/controller/ProductController.js
@@ -20,5 +20,17 @@ module.exports = {
         return res.json(product);
     },
 
+    async update(req, res){
+        const product = await Product.findByIdAndUpdate(req.params.id, req.body, {new: true});
+
+        return res.json(product);
+    },
+
+    async destroy(req, res){
+        await Product.findByIdAndRemove(req.params.id);
+
+        return res.send();
+    },
+
     
 }
--- src/routes.js
+++ src/routes.js
@@ -4,8 +4,10 @@ const PrductController = require("./controller/ProductController")
 
 
 routes.get('/products', PrductController.index );
-routes.get('/products/:id', PrductController.show);
+routes.get('/products/:id', PrductController.show );
 routes.post('/products', PrductController.store );
+routes.put('/products/:id', PrductController.update );
+routes.delete('/products/:id', PrductController.destroy );
 
 
 module.exports = routes;
