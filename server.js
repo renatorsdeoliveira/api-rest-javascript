f662cd343c7b9a97b91225da8fb2288387c90328
renato_O <renatorsdeoliveira@gmail.com>
2020-08-30 13:33:29 -0300

    Organizado a parte das Rotas

--- server.js
+++ server.js
@@ -11,21 +11,10 @@ const app = express();
 mongoose.connect("mongodb://localhost:27017/nodeapi", { useNewUrlParser: true,  useUnifiedTopology: true});
 //require('./src/models/Product'); // chamando o modulo manual
 requireDir('./src/models/') // chamando o modulo automatica com require-dir
-const Product = mongoose.model("Product");
 
 
-// Primeira Rota
-app.get('/', (req, res) => {
-    Product.create({
-        title: "React Native",
-        description: "Build native apps with React",
-        url: "http://github.com/facebook/react-native"
-    });
-
-
-    return res.send("Hello teste");
-})
-
+// Rotas
+app.use('/api', require('./src/routes') );
 
 
 // Porta
---
+++ src/routes.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const routes = express.Router()
+
+
+routes.get('/', (req, res) => {
+    // Product.create({
+    //     title: "React Native",
+    //     description: "Build native apps with React",
+    //     url: "http://github.com/facebook/react-native"
+    // });
+
+
+    return res.send("Hello teste");
+})
+
+module.exports = routes;
