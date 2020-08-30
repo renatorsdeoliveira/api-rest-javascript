85aafd95b30c7852841dc095c9d38c54139a82a4
renato_O <renatorsdeoliveira@gmail.com>
2020-08-30 13:08:32 -0300

    Instalando o require-dir para fazer os requires automaticos e feito a criação do produto inserindo no banco

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
-const express = require("express")
-const mongoose = require("mongoose")
+const express = require("express");
+const mongoose = require("mongoose");
+const requireDir = require("require-dir");
 
 
 // Iniciando o APP
@@ -8,12 +9,21 @@ const app = express();
 
 // Iniciando o DB
 mongoose.connect("mongodb://localhost:27017/nodeapi", { useNewUrlParser: true,  useUnifiedTopology: true});
-require('./src/models/Product');// chamando o modulo
+//require('./src/models/Product'); // chamando o modulo manual
+requireDir('./src/models/') // chamando o modulo automatica com require-dir
+const Product = mongoose.model("Product");
 
 
 // Primeira Rota
 app.get('/', (req, res) => {
-    res.send("Hello teste");
+    Product.create({
+        title: "React Native",
+        description: "Build native apps with React",
+        url: "http://github.com/facebook/react-native"
+    });
+
+
+    return res.send("Hello teste");
 })
 
 
