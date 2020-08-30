b96086630c8eed0a70da07b31c5143d71eb30b59
renato_O <renatorsdeoliveira@gmail.com>
2020-08-30 17:59:59 -0300

    Criaado a requisição por um id específico

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ const PrductController = require("./controller/ProductController")
 
 
 routes.get('/products', PrductController.index );
+routes.get('/products/:id', PrductController.show);
 routes.post('/products', PrductController.store );
 
 
